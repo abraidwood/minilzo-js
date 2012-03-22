cc7bdc54bff6e8712e2a94542554ce5ea60f7a58
Alistair Braidwood <alistair_braidwood@yahoo.co.uk>
2012-03-22 20:30:52 +0000

    bojt.js license

--- examples/node/bojt.js
+++ examples/node/bojt.js
@@ -19,6 +19,11 @@
  *
  * unBOJ supports recursive unBOJing (recursive BOJing is crazy)
  *
+ * ------
+ * This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
  */
 
 String.prototype.splice = function( idx, rem, s ) {
